cac9dfba5af2755d8a6106b452934d976ec5cc2e
Whatisinternet <joshteeter@gmail.com>
2015-03-07 17:22:30 -0500

    Minor issue with generator hopefully

--- lib/generators/spark_plugs/install_generator.rb
+++ lib/generators/spark_plugs/install_generator.rb
@@ -3,7 +3,6 @@ require 'rails/generators'
 module SparkPlugs
   class InstallGenerator < Rails::Generators::Base
     source_root File.expand_path('../templates',__FILE__)
-    include Generators::Utils::InstanceMethods
 
     argument :_namespace, type: :string, required: false, desc: 'SparkPlug namespace'
 
