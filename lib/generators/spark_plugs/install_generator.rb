89c957086c552f0e49a7fa2e368ed8c5a5566707
Whatisinternet <joshteeter@gmail.com>
2015-03-07 17:17:11 -0500

    Added generators

---
+++ lib/generators/spark_plugs/install_generator.rb
@@ -0,0 +1,18 @@
+require 'rails/generators'
+
+module SparkPlugs
+  class InstallGenerator < Rails::Generators::Base
+    source_root File.expand_path('../templates',__FILE__)
+    include Generators::Utils::InstanceMethods
+
+    argument :_namespace, type: :string, required: false, desc: 'SparkPlug namespace'
+
+    def install
+      namespace = ask_for("Where would you like to mount spark_plugs?", 'admin', _namespace)
+      route("mount SparkPlugs::Engine => '/#{namespace}", as: 'spark_plugs')
+      rake("db:migrate")
+      #template 'initializer.erb', 'config/initializers/spark_plugs.rb'
+    end
+
+  end
+end
--- lib/tasks/spark_plugs_tasks.rake
+++ lib/tasks/spark_plugs_tasks.rake
@@ -2,3 +2,14 @@
 # task :spark_plugs do
 #   # Task goes here
 # end
+namespace :spark_plugs do
+  desc 'Install spark_plugs'
+  task :install do
+    system 'rails g spark_plugs:install'
+  end
+
+  desc 'Uninstall spark_plugs'
+  task :uninstall do
+    system 'rails g spark_plugs:uninstall'
+  end
+end
